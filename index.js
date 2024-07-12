69bd84cb7f53d4b1954cf6a3e776b6f5a3baec69
Shafiul Alam <shafiulalam0307@gmail.com>
2024-07-12 14:35:44 +0600

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login":"Shafiul alma",
+  "id":1904006,
+  "node_id":"shafiu1"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,6 +18,9 @@ app.get('/twitter',(req,res)=>{
     res.send("hiteshdotcom")
 })
 
+app.get('/github',(req,res)=>{
+  res.json(githubData)
+})
 app.get('/login',(req,res)=>{
     res.send('<h1>Please login at Shafiul world</h1>')
 
